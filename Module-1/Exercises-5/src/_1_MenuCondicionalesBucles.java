a1f22967768f7572e9bb58d0736438e621cbd99d
xdmrk <alejandra.villaloboss3@gmail.com>
2025-07-12 13:16:23 -0500

    Identacion mal

--- Module-1/Exercises-5/src/_1_MenuCondicionalesBucles.java
+++ Module-1/Exercises-5/src/_1_MenuCondicionalesBucles.java
@@ -1,6 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("fsdf");
+import java.util.Scanner;
+
+public class _1_MenuCondicionalesBucles {
+    public static void main(String[] args) {
+        /*
+         * menu de opciones
+         * 
+         */
+
+
+        System.out.println("\n¡¡  MENU MATEMATICO  !!\n");
+        System.out.println("1. Calcular área de una figura");
+        System.out.println("2. Calcular edad futura");
+        System.out.println("3. ¿Sí es numero primo?");
+        System.out.println("0. Salir\n");
+
+        int opcion;
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.print("Ingrese una opcion: ");
+        opcion = entrada.nextInt();
+
     }
 }
