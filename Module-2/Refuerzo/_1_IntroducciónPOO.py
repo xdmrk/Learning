06e1d3943e081fbfd01914131a0fad6a85d16f23
xdmrk <alejandra.villaloboss3@gmail.com>
2025-07-16 18:01:40 -0500

    Save

--- Module-2/Refuerzo/_1_IntroducciónPOO.py
+++ Module-2/Refuerzo/_1_IntroducciónPOO.py
@@ -6,5 +6,5 @@ print(type(numero))
 int #Ctrl + int (Ingresamos a la clase int): en palabras reservadas
 # int. muestra todos los metodos de la clase entero
 
-cadena = "Hola mundo, Hello Word".split()
-print(cadena)   #
+cadena = "Hola mundo, Hello Word".split() 
+print(cadena)   #Salida: ['Hola', 'mundo,', 'Hello', 'Word']
