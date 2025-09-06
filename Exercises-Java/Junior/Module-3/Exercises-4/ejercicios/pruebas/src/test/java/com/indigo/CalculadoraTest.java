14ce9ffb1d5923932af9345329d07a0a0110fc33
xdmrk <alejandra.villaloboss3@gmail.com>
2025-09-05 22:28:05 -0500

    lambda

--- Exercises-Java/Junior/Module-3/Exercises-4/ejercicios/pruebas/src/test/java/com/indigo/CalculadoraTest.java
+++ Exercises-Java/Junior/Module-3/Exercises-4/ejercicios/pruebas/src/test/java/com/indigo/CalculadoraTest.java
@@ -1,6 +1,38 @@
-package test.java.com.indigo;
+package com.indigo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
     @Test
+    void testSuma() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.sumar(2, 3));
+    }
+    @Test
+    void testResta() {
+        Calculadora calc = new Calculadora();
+        assertEquals(0, calc.restar(2, 3));
+    }
+    @Test
+    void testDivi() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.dividir(10, 2));
+    }
+    @Test
+    void testMul() {
+        Calculadora calc = new Calculadora();
+        assertEquals(6, calc.multiplicar(2, 3));
+    }
+
+    @Test
+    void testDiviPorCero() {
+        Calculadora calc = new Calculadora();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calc.dividir(6, 0);
+        });
+    }
 
 }
