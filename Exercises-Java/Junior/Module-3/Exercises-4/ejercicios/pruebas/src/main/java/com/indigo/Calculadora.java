d569f47bbed1508036c81bf89fb99057b1d6dfa5
xdmrk <alejandra.villaloboss3@gmail.com>
2025-09-05 21:53:03 -0500

    pruebas

---
+++ Exercises-Java/Junior/Module-3/Exercises-4/ejercicios/pruebas/src/main/java/com/indigo/Calculadora.java
@@ -0,0 +1,22 @@
+package main.java.com.indigo;
+
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+}
---
+++ Exercises-Java/Junior/Module-3/Exercises-4/ejercicios/pruebas/src/test/java/com/indigo/CalculadoraTest.java
@@ -0,0 +1,6 @@
+package test.java.com.indigo;
+
+public class CalculadoraTest {
+    @Test
+
+}
